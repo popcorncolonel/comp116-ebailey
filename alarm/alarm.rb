7e3ace3e249b01f90627c1e8f228352a3c08b58e
popcorncolonel <popcorncolonel@gmail.com>
2014-09-27 13:35:30 -0400

    even more code outline

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,31 +1,68 @@
 #!/usr/bin/ruby
 
+def live_capture()
+=begin
+    ***MUST DETECT***
+NULL scan
+Xmas scan
+Credit card number leaked in the clear via website
+
+***IN THE FORM***
+#{incident_number}. ALERT: #{attack} is detected from #{source IP address} (#{protocol}) (#{payload})!
+
+OR
+
+#{incident_number}. ALERT: Credit card leaked in the clear from #{source IP address} (#{protocol}) (#{payload})!
+
+=end
+    puts ("TODO :D")
+end
+
 def analyze_log(filename)
 =begin
     **NOTES** (about access.log)
-    * tons and tons of the same thing together (trying to login to wp hundreds of times per minute) => not good guy
-        * yeah. line 12879 - hundreds of 404's per second.
-        * In general: 1 IP, thousands of things per second => bad
-    * People whose first requests are /robots.txt are generally good?
-        * stuff with \*bot/[0-9].[0-9] in the user agent are generally good (bingbot, googlebot, yandexbot)
     * if the user agent is from nmap then... yeah... cmon...
-    * Anything with /scanner/i in the user-agent is no.
-    * IPs from whom the majority of requests have response 2\*\* are generally good?
-    * stuff coming from w3af.org... probably not good
+
+    ***MUST DETECT***
+NMAP scan (of any variety)
+HTTP errors, anything that has an HTTP status code in the 400-range
+Shellcode. For some background reading on shellcode and what it is, read https://morgawr.github.io/hacking/2014/03/29/shellcode-to-reverse-bind-with-netcat/
+
+***IN THE FORM***
+#{incident_number}. ALERT: #{attack} is detected from #{source IP address} (#{protocol}) (#{payload})!
+
 =end
+
+    def read_incident(line)
+=begin
+***LINE FORMAT***
+73.38.0.142 - - [11/Sep/2014:23:49:48 +0200] "HEAD /import HTTP/1.1" 404 0 "-" "Mozilla/5.0"
+
+=TOKENS=
+
+IP - - [date:time +tz] "request" statuscode ? ? "user-agent"
+=end
+        #if it's an nmap scan (how 2 detect?) (just see if line is coming from nmap? is there a way it's coming from an nmap scan but not from user-agent nmap?)
+        #if it has a 4** http response (easy. regex.)
+        #if it's shellcode
+        to_return = nil
+        #tokenize based on spaces " then by spaces
+    end
+
     f = File.open(filename, 'r')
     puts "reading..."
+    incident = 0
     f.each_line do |line|
-        #MAIN LOOP. ANALYZE THE LOG HERE.
-        #puts line
+        line = read_incident(line)
+        if line != nil
+            puts "better formatted string than this one. " + line
+            incident += 1
+        end
     end
     puts "done reading"
     f.close
 end
 
-def live_capture()
-    puts ("TODO :D")
-end
 
 flag = ARGV[0]
 filename = ARGV[1]
