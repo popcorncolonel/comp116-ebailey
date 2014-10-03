f162519b55a1853627f00f482e7735585c48415d
popcorncolonel <popcorncolonel@gmail.com>
2014-10-03 17:17:54 -0400

    Log reader appears to be done.

--- alarm/README.md
+++ alarm/README.md
@@ -5,13 +5,13 @@ COMP116 HW2**
 ---
 
 **What has been implemented**  
-Nothing yet
+So far, the log reader has been implemented to the best of my knowledge.
 
 **Collaborators/people I've discussed it with**  
-No one so far
+I have not discussed this problem.
 
 **Hours taken**  
-0 so far
+2
 
 **Are the heuristics used in this assignment to determine incidents "even that good"?**
 
@@ -20,18 +20,12 @@ No one so far
 
 
 #TODO#
-* write log analyzer
 * write live alarm
-* remove notes
+* remove (some) notes
 * Ask this question: What do we do if there is more than one thing wrong with the log? (ex. shellcode and 4\*\* error) Do we print each error? Just the first one we hit?
 * remove sheep?
 
 ---
-
-**MUST DETECT**
-* NMAP scan (of any variety)
-* HTTP errors, anything that has an HTTP status code in the 400-range
-* Shellcode. 
 
 **NOTES** (about access.log)
 * user-agent from nmap
--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -26,6 +26,11 @@ OR
         stream.save
         stream.stream.each do |p|
             packet = ::PacketFu::Packet.parse(p)
+            #if NULL scan then end
+            #if Xmas scan then end
+            #if Credit Card then end #
+            #CREDIT CARD REGEX: /(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/
+            #- http://stackoverflow.com/questions/9315647/regex-credit-card-number-tests
             puts packet.peek
             puts "==============================================================================="
             puts packet.payload
@@ -62,50 +67,53 @@ def analyze_log(filename)
     $incident = 1
 
     def alert(attack, ip_addr, protocol, payload)
-        attack = attack
         puts '%d. ALERT: %s is detected from %s (%s) ("%s")!' %[$incident, attack, ip_addr, protocol, payload]
+        #if attack == 'NMAP scan' then
+        #    puts '%d. ALERT: %s is detected from %s (%s) ("%s")!' %[$incident, attack, ip_addr, protocol, payload]
+        #end
         $incident += 1
     end
 
     def has_shellcode?(str)
         #shellcodes appear to be of the form /(\\x[0-9a-f]{2}.{0,1}){3,}/i
         #AND at the beginning of strings!
-        return str =~ /(\\x[0-9a-f]{2}.{0,1}){3,}/i
+        return (str =~ /(\\x[0-9a-f]{2}.{0,1}){3,}/i) == 0 #aka if it's at the beginning of the line
     end
     
     def analyze_log_line(line)
         error_statement = nil
 
+        protocol = nil
         ip_addr = line.split(' ')[0]
         request = line.split('"')[1]
         status_code = line.split('"')[2].split(' ')[0]
         user_agent = line.split('"')[5]
 
-        if request != '' then #some lines are like this o.O with 400 errors
-            if has_shellcode?(request) == 0 then
-                alert('shellcode', ip_addr, 'HTTP', request)
-                return
+
+        if request != '' then #some lines are like this with no payloads o.O with 400 errors
+            #shellcode
+            if has_shellcode?(request) then
+                protocol = 'HTTP'
+                alert('Shellcode', ip_addr, protocol, request)
             end
-            #alert(incident, 'HTTP error', ip_addr, protocol, payload)
-            # if request is shellcode, alert
-            protocol = request.split(' ')[-1].split('/')[0]
-            if protocol != 'HTTP' and false then
-                puts
-                puts
-                puts protocol
-                puts line
-                puts
-                puts
+
+            if protocol == nil then
+                protocol = request.split(' ')[-1].split('/')[0]
             end
-        else
-            #puts status_code
+            if protocol != 'HTTP' and protocol != 'RTSP' and protocol != 'HTTPS' then
+                protocol = 'HTTP'
+            end
+        end
+
+        #nmap
+        if line =~ /nmap/ then 
+            alert('NMAP scan', ip_addr, protocol, request)
         end
 
         #bad status code
         if status_code.to_i >= 400 then
-            #alert(incident, 'HTTP error', ip_addr, protocol, payload)
+            alert('HTTP error', ip_addr, protocol, request)
         end
-
 
     end
 
